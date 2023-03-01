45f5184d8035a571ce0dc8805944879cfd4d4d0a
Lev_Semin <semin.leva12@mail.ru>
2023-03-01 18:39:29 +0300

    fix

--- src/test_copy_move_semantic/test_move_semantic.hpp
+++ src/test_copy_move_semantic/test_move_semantic.hpp
@@ -7,31 +7,19 @@
 template<class T>
 SL::SuperType<T> sum(SL::SuperType<T> a, SL::SuperType<T> b)
 {
-    start_function()
-    
-    a.rename("func_a");
-    b.rename("func_b");
-
     SL::SuperType<int> c = a + b;
-    c.rename("c");
    
-    end_function()
    
     return c;
 }
 
 void test_move_semantic()
 {
-    start_function();
     
     SL::SuperType<int> a(5);
-    a.rename("a");
     SL::SuperType<int> b(14);
-    b.rename("b");
     SL::SuperType<int> c(20);
-    c.rename("c");
     SL::SuperType<int> result(0);
-    result.rename("result");
 
     result = sum(a, b * c);
     
